98ef7dcbbd0862e645c495e80b682ebce3b5d7d3
ryan <woden6969696969@hotmail.com>
2025-08-29 13:41:49 +1000

    1

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => ['http://localhost:5173', 'https://modu18.vip', 'https://18modu.vip', 'https://jm69.vip'],
 
     'allowed_origins_patterns' => [],
 
