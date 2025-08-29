91cd60ee718e883105193372a99fd9c5e4e87eb9
ryan <woden6969696969@hotmail.com>
2025-08-29 13:37:01 +1000

    1

--- config/database.php
+++ config/database.php
@@ -172,6 +172,16 @@ return [
 
             'persistent_id' => 'cache',
         ],
+        'session' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_SESSION_DB', '2'),
+
+            'persistent_id' => 'session',
+        ]
 
     ],
 
