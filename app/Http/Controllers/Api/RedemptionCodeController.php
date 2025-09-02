8e279456df221fd15ce38f34a8579c7330b95e82
ryan <woden6969696969@hotmail.com>
2025-09-02 23:08:29 +1000

    1

--- app/Http/Controllers/Api/RedemptionCodeController.php
+++ app/Http/Controllers/Api/RedemptionCodeController.php
@@ -39,7 +39,7 @@ class RedemptionCodeController extends Controller
             if ($exists) {
                 return response()->json([
                     'success' => false,
-                    'message' => '30天内只能兑换最多只能创建1个兑换码'
+                    'message' => '30天内只能创建1个兑换码'
                 ], 200);
             }
         }
