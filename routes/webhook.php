39ae42bf5429152ec85ba148516c6de718e08c64
ryan <woden6969696969@hotmail.com>
2025-09-03 14:59:56 +1000

    1

--- routes/webhook.php
+++ routes/webhook.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([])->namespace('App\Http\Controllers\Webhook')->group(function () {
-    Route::post('/api/receive-mch', 'MchPaymentWebhookWebController@receivePayment')->name('payment.receive-mch.a');
-});
-
-Route::middleware([])->namespace('App\Http\Controllers\Webhook')->group(function () {
     Route::post('/receive-mch', 'MchPaymentWebhookWebController@receivePayment')->name('payment.receive-mch');
 });
 
