21eb77fd026a7573aa5031d3b558635cfbe195b7
ryan <woden6969696969@hotmail.com>
2025-09-03 15:01:55 +1000

    1

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,3 @@ use App\Http\Controllers\Webhook\MchPaymentWebhookWebController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Payment webhook routes
-Route::post('/webhook/receive-mch', [MchPaymentWebhookWebController::class, 'receivePayment'])
-    ->name('webhook.mch.payment')
-    ->withoutMiddleware(['web', 'csrf']);
