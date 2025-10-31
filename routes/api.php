baf531a7649b8423695907d325fa2aec4643fc42
ryan <woden6969696969@hotmail.com>
2025-10-31 18:08:00 +1100

    video finished

--- routes/api.php
+++ routes/api.php
@@ -61,6 +61,8 @@ Route::get('/comic/{id}', [\App\Http\Controllers\Api\RandomComicsController::cla
 // Online Video Routes (public - guest can view list and settings)
 Route::get('/online-video', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'get'])->name('online.video');
 Route::get('/online-video-settings', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'setting'])->name('online.video.setting');
+Route::get('/online-video/{id}/detail', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'getVideoDetail'])->name('online.video.detail');
+Route::get('/online-video/hot-recommendations', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'getHotRecommendations'])->name('online.video.hot');
 
 // Test decrypt endpoints (public for testing)
 Route::prefix('test')->group(function () {
@@ -114,9 +116,6 @@ Route::middleware('auth:sanctum')->group(function () {
     // Affiliate dashboard
     Route::get('/affiliate/dashboard', [\App\Http\Controllers\Api\AffiliateController::class, 'dashboard']);
 
-    // Video detail and recommendations (requires auth)
-    Route::get('/online-video/{id}/detail', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'getVideoDetail'])->name('online.video.detail');
-    Route::get('/online-video/hot-recommendations', [\App\Http\Controllers\Api\Video\OnlineVideoApiController::class, 'getHotRecommendations'])->name('online.video.hot');
 });
 
 // Admin routes (requires auth and admin role)
